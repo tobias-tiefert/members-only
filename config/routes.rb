3b57357b97568f3a72bd1641725066e5deb02e08
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-20 16:33:44 +0200

    Add Login and Logout buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :posts
+  resource :sign_up
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
