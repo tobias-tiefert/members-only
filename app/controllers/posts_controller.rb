9c25c9f7ffaca1d7e3a55b34a31d843499904c89
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-21 09:34:34 +0200

    Add creating and viewing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,31 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-  allow_unauthenticated_access
-  # only: %i[ index ]
+  allow_unauthenticated_access only: %i[ index show ]
   def index
     @posts = Post.all
+    @username = Current.user.username if authenticated?
   end
 
   def show
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def edit
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = Current.user.id
+
+    if @post.save
+      redirect_to @post
+    else
+      flash.now[:alert] = "Post couldn't be saved."
+      render :new, status: :unprocessable_content
+    end
   end
 
   def update
